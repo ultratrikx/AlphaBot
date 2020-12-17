ef20de9d56e552c285d39b6283b0592c2464f555
Rohanth Marem <rohanthmarem@Rohanths-iMac.local>
2020-12-16 21:19:07 -0500

    one final fix to ping cmnd

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send(`hi there, now shut up\n\n`
+        message.channel.send(`hi there, now shut up\n`
             + `anyways, heres the ping; 🏓 ${Math.round(message.client.ws.ping)} ms`)
     }
 }
