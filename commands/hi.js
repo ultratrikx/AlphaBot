edbb9bafadea2d115bd0b3eb3f7c46a2e8f29967
Rohanth Marem <rohanthmarem@Rohanths-iMac.local>
2020-12-16 21:16:58 -0500

    ping times shows up now

--- commands/hi.js
+++ commands/hi.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'hi',
     description: "this is hi command!",
     execute(message, args){
-        message.channel.send('hi there, now shut up');
+        message.channel.send('hi im baby kata 2, nice to meet you. may i haunt you in your dreams?');
     }
 }
--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send('hi im baby kata 2, nice to meet you. may i haunt you in your dreams?');
+        message.channel.send(`hi there, now shut up\n\n`
+            + `anyways, heres the ping; 🏓 ${Math.round(message.client.ws.ping)} ms`)
     }
 }
