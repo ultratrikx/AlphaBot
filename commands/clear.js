46a69a63431898e37de5cf8fc7b86fa4578575cd
Rohanth Marem <rohanthmarem@Rohanths-iMac.local>
2020-12-17 19:23:45 -0500

    im bad at code

--- commands/clear.js
+++ commands/clear.js
@@ -3,15 +3,15 @@ module.exports = {
     description: "this is a clear command!",
     async execute(message, args){
 
-        if(message.member.roles.cache.has('764586697106522147', '689142040965152778', '665035759861891072')){
+        
             if(!args[0]) return message.reply("enter a number of messages to clear");
             if(isNaN(args[0])) return message.reply("enter a number following the command like so `.clear [1]`");
 
             if(args[0] > 100) return message.reply("the maximum amount of messages you can delete is 100");
             if(args[0] < 1) return message.reply("delete at least 1 message");
         
-        
-            await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
+            if(message.member.roles.cache.has('764586697106522147', '689142040965152778', '665035759861891072')){
+              await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
                 message.channel.bulkDelete(messages);
                 message.reply('Done').then((m) => m.delete({ timeout: 500 }));
         } 
