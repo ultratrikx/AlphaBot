7533a17d39fdfcbdc62dba360928de84280e81df
Rohanth Marem <rohanthmarem@Rohanths-iMac.local>
2020-12-25 10:53:30 -0500

    1.4.1 with slash commands

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Commands')
-        .setAuthor('Alpha Bot#0038 (aka baby kata 2)', 'https://cdn.discordapp.com/avatars/702514788340858892/d72991959325a20107bb0efb61118361.png?size=1024')
+        .setAuthor('Alpha Bot#0038', 'https://cdn.discordapp.com/avatars/702514788340858892/d72991959325a20107bb0efb61118361.png?size=1024')
         .setDescription('Here’s the list of commands!\n\n'
             + '**Normal Commands**\n'
             + 'type `.ping` to get rudely insulted\n'
@@ -13,8 +13,8 @@ module.exports = {
             + 'type `.babykata2` for meme\n'
             + 'type `.weather [location]` for weather of said location\n'
             + 'type `.covid [country]` for info about their covid cases\n'
-            + 'type `.math [mathequation]` for calculation\n'
-           // + 'type `.meme` for a meme (they may contain swearing)\n'
+            + 'type `.math [math equation]` for calculation\n'
+            + 'type `.meme` for a meme (they may contain swearing)\n'
             + 'type `.botinfo` for information about the bot\n\n'
             + '**Admin Commands**\n'
             + 'type `.clear [number]` to clear messages\n'
@@ -32,7 +32,7 @@ module.exports = {
             + 'type `.boostercolour` to get the booster pink colour (boost this server to unlock this)')
 
             
-        .setFooter('Made by nf#0001 in partnership with ultratrikx#1056 and Alpha Bot#0038 (aka baby kata 2)');
+        .setFooter('Made by nf#0001 in partnership with ultratrikx#1056 and Alpha Bot#0038');
 
         message.channel.send(newEmbed);
     }
--- main.js
+++ main.js
@@ -19,6 +19,81 @@ for (const file of commandFiles){
 
 client.once('ready', () => {
     console.log('AlphaBot is online!');
+
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: "ping",
+            description: "Shows bot's ping"
+        }
+    });
+
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: "help",
+            description: "Shows a list of all commands for AlphaBot"
+        }
+    });
+
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        const command = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+
+        if(command == 'ping') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 3,
+                    data: {
+                        content: `hi there, now shut up\n`
+                            + `anyways, heres the ping; 🏓 ${Math.round(client.ws.ping)} ms`
+                    }
+                }
+            });
+        }
+
+        if(command == 'help') {
+            const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Commands')
+            .setAuthor('Alpha Bot#0038', 'https://cdn.discordapp.com/avatars/702514788340858892/d72991959325a20107bb0efb61118361.png?size=1024')
+            .setDescription('Here’s the list of commands!\n\n'
+                + '**Normal Commands**\n'
+                + 'type `.ping` to get rudely insulted\n'
+                + 'type `.hi` to get haunted\n'
+                + 'type `.babykata2` for meme\n'
+                + 'type `.weather [location]` for weather of said location\n'
+                + 'type `.covid [country]` for info about their covid cases\n'
+                + 'type `.math [math equation]` for calculation\n'
+                + 'type `.meme` for a meme (they may contain swearing)\n'
+                + 'type `.botinfo` for information about the bot\n\n'
+                + '**Admin Commands**\n'
+                + 'type `.clear [number]` to clear messages\n'
+                + 'type `.mute [@user] [number]m or s` to timed mute mentioned user \n'
+                + 'type `.unmute [@user]` to manually unmute the mentioned user\n '
+                + 'type `.kick [@user]` to kick mentioned user\n'
+                + 'type `.ban [@user]` to punish someone who says trans right aren‘t human rights\n\n'
+                + '**Colour Role Commands**\n'
+                + 'type `.redrole` to get a red colour on your name\n'
+                + 'type `.blues` to get the blue colour on you name\n'
+                + 'type `.grebn` to become shrek\n'
+                + 'type `.pruple` to become pruple\n'
+                + 'type `.rameningscolour` to cause confuse\n'
+                + 'type `.admincolour` to get the black admin colour (only for admins you idiot)\n'
+                + 'type `.boostercolour` to get the booster pink colour (boost this server to unlock this)')
+    
+                
+            .setFooter('Made by nf#0001 in partnership with ultratrikx#1056 and Alpha Bot#0038');
+
+            
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await createAPIMessage(interaction, embed)
+                }
+            });
+        }
+
+    });
+
     client.user.setPresence({
         status: "dnd",  //You can show online, idle... Do not disturb is dnd
         
@@ -29,13 +104,20 @@ client.once('ready', () => {
     });
 });
 
+async function createAPIMessage(interaction, content) {
+    const apiMessage = await Discord.APIMessage.create(client.channels.resolve(interaction.channel_id), content)
+        .resolveData()
+        .resolveFiles();
+    
+    return { ...apiMessage.data, files: apiMessage.files };
+}
+
 client.on('guildMemberAdd', guildMember =>{
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
 
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('657402007925030925').send(`🎉 Welcome <@${guildMember.user.id}> <:smrik:771347389055107102> lol`)
 })
-
 
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot)return;
