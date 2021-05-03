eac4943da00afb6b912da6158448ab2b58fe8da5
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-05-03 09:11:18 -0400

    1.5.14a bug fix

--- commands/aa.js
+++ commands/aa.js
@@ -1,39 +1,58 @@
-const gameone = new Discord.MessageEmbed()
-    .setColor('#535ded')
-    .setTitle(`${visitingteam} @ ${hometeam}`)
-    .setDescription(`In ${arena}, ${location}`)
-    .setAuthor(`Game ID: ${gameid}`)
-    .setTimestamp();
+module.exports = {
+    name: 'aa',
+    description: 'makes your text big',
 
-if (gameover !== 'false') {
-    gameone.addField('**Final**', '_ _');
-} else {
-    gameone.addField(`Tip Off @`, time);
-}
-gameone.addFields(
-    {
-        name: `${teamLogo[visitingteam]}  ${visitingteam} \`${vwins} - ${vloss}\``,
-        value: `${vteamscore} PTS`,
-        inline: true,
+    async execute(client, message, args, Discord) {
+        const gameone = new Discord.MessageEmbed()
+            .setColor('#535ded')
+            .setTitle(`${visitingteam} @ ${hometeam}`)
+            .setDescription(`In ${arena}, ${location}`)
+            .setAuthor(`Game ID: ${gameid}`)
+            .setTimestamp();
+
+        if (gameover !== 'false') {
+            gameone.addField('**Final**', '_ _');
+        } else {
+            gameone.addField(`Tip Off @`, time);
+        }
+        gameone.addFields(
+            {
+                name: `${teamLogo[visitingteam]}  ${visitingteam} \`${vwins} - ${vloss}\``,
+                value: `${vteamscore} PTS`,
+                inline: true,
+            },
+            {
+                name: `${teamLogo[hometeam]}  ${hometeam} \`${hwins} - ${hloss}\``,
+                value: `${hteamscore} PTS`,
+                inline: true,
+            },
+        );
+        if (
+            gameover == 'false' &&
+            period > 0 &&
+            periodend == 'true'
+        ) {
+            gameone.addField(`End of Q${period}`, '_ _');
+        }
+        if (gameover == 'false' && period > 0 && halftime == 'true') {
+            gameone.addField(`Halftime`, '_ _');
+        }
+        if (
+            gameover == 'false' &&
+            period > 0 &&
+            halftime == 'false' &&
+            periodend == 'false'
+        ) {
+            gameone.addField(`🔴 Q${period} - ${clock}`, '_ _');
+        }
+        gameone.addField(
+            `Broadcasters`,
+            `${vbroadcast}, ${hbroadcast}`,
+        );
+
     },
-    {
-        name: `${teamLogo[hometeam]}  ${hometeam} \`${hwins} - ${hloss}\``,
-        value: `${hteamscore} PTS`,
-        inline: true,
-    },
-);
-if (gameover == 'false' && period > 0 && periodend == 'true') {
-    gameone.addField(`End of Q${period}`, '_ _');
-}
-if (gameover == 'false' && period > 0 && halftime == 'true') {
-    gameone.addField(`Halftime`, '_ _');
-}
-if (
-    gameover == 'false' &&
-    period > 0 &&
-    halftime == 'false' &&
-    periodend == 'false'
-) {
-    gameone.addField(`🔴 Q${period} - ${clock}`, '_ _');
-}
-gameone.addField(`Broadcasters`, `${vbroadcast}, ${hbroadcast}`);
+};
+
+
+
+
