71e9d4e00dd5140057d98b188f723d6c60d108ec
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-05-19 15:13:27 -0400

    1.5.15, slomo command update, giveaway command fix

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const ezlocalTime = require('ez-local-time');
 
 const client = new Discord.Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
@@ -9,16 +8,14 @@ const prefix = '.';
 
 const fs = require('fs');
 
-//const { token } = require('./security/config.json');
+// const { token } = require('./security/config.json');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-/*
-['command_handler', 'event_handler'].forEach((handler) => {
-    require(`./handlers/${handler}`)(client, Discord);
-});
-*/
+// ['command_handler', 'event_handler'].forEach((handler) => {
+//     require(`./handlers/${handler}`)(client, Discord);
+// });
 
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
