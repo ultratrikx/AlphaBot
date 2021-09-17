86256f1693156fa12e8af41f6109dc6571830f9f
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-09-17 19:56:17 -0400

    3.00 bug fixes

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ client.slashCommands = new Collection();
 client.config = require("./config/config.json");
 
 
-    const { GiveawaysManager } = require('discord-giveaways');
-    client.giveawaysManager = new GiveawaysManager(client, {
-        storage: './giveaways.json',
-        updateCountdownEvery: 5000,
-        default: {
-            botsCanWin: false,
-            embedColor: '#ff8cff',
-            reaction: '🎉',
-        },
-    });
 
 // Initializing the project
 require("./handler")(client);
