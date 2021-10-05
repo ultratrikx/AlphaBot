e391e21dda39f27dad2a7748128230d73d45af48
ultratrikx <64563315+ultratrikx@users.noreply.github.com>
2021-10-05 17:24:41 -0400

    ad

--- commands/Info/ad.js
+++ commands/Info/ad.js
@@ -10,7 +10,7 @@ module.exports = {
      */
     run: async (client, message, args) => {
         const channelToSend = client.channels.cache.get('888805141631885412') //796140427878858763
-        channelToSend.send(`test`); // https://cdn.discordapp.com/attachments/856543669938946048/895058206617911306/unnamed.gif
+        channelToSend.send(`https://cdn.discordapp.com/attachments/856543669938946048/895058206617911306/unnamed.gif`);
     },
 };
 
